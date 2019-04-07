605cee01bc33f414b48c4c5ac4aa63dfd4f44b02
afghanimah <afghanimah@gmail.com>
2019-04-06 20:37:24 -0400

    connection strings should swap automatically

--- base.py
+++ base.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ['DATABASE_URL']
+# BACKUP_DATABASE_URL is where you can put your localhost db connection string for example.
+# os.environ.get("DATABASE_URL") will get the connection string for the actual db from your hosting service.
+# If you're running locally, the backup connection string will be used instead assuming
+# that you don't haven't set DATABASE_URL yourself.
+BACKUP_DATABASE_URL = "postgresql://localhost/restfulchat"
+DATABASE_URL = os.environ.get("DATABASE_URL", BACKUP_DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
